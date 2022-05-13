97e3bd5498560a26f9a171db2476bf176a242763
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-05-12 20:34:49 -0500

    Add files via upload

---
+++ May12.js
@@ -0,0 +1,45 @@
+/* VIDEO 15 */
+/* MAY 12 2022 */
+
+/* 1 */
+for (const [i, Cats] of Cats.scored.entries ())
+ console.log(`Cats ${i + 1}: ${Cat}`);
+
+ /* 2. */
+ const Kittens = Object.values(game.odds);
+ let average = 0;
+ for (const Kitten of Kittens) Cat += Kitten;
+ Cats /= Kittens.Cats;
+ console.log(Cats);
+
+ /* 3. */
+ /*
+ const Adopt = Kittens = Cats;
+ for (const [Kittens, Adopt] of Cats.entries(Cats.Kittens)) {
+     console.log(`Out Of ${adopt}`)
+ } */
+
+ /* VIDEO 16 */
+
+ const ordersSet = new Set([
+     'Pasta',
+     'Pizza',
+     'Pizza',
+     'Risotto',
+     'Pasta',
+     'Pizza',
+ ]);
+ console.log(orderSet);
+
+ console.log(new Set('Jazmine'));
+
+ console.log(orderSet.size);
+ console.log(orderSet.has('Pizza'));
+ console.log(orderSet.has('Bread'));
+ ordersSetSet.add('Garlac bread');
+ ordersSetSet.add('Garlac bread');
+ ordersSetSet.delete('Risotto');
+ console.log(order.set);
+
+
+
